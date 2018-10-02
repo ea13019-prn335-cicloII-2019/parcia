650f5fb98d11678ae8fa6f4dd12762880a4a0d50
ernestoestrada <37189144+ea13019@users.noreply.github.com>
2018-10-02 09:55:11 -0600

    Add files via upload

---
+++ parcialrepetido.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+//llenado de matriz
+int main()
+{
+	int matriz[3][3];
+	int matrizb[3][3];
+	int filas, columnas, filas2, columnas2;
+	int dato;
+	
+	
+	//Llenar matriz
+	printf("Ingrese los datos a la matriz\n");
+	for(int f=0;f<filas;f++)
+	{
+		for(int c =0;c<columnas;c++)
+		{
+			scanf("%d", &dato);
+			//metodo de la burbuja
+		}
+	}
+			int num;
+			int m,n;
+			
+			
+			printf("ingrese los datos a la matrizb\n");
+			scanf("%d",
+			
+			for(int m=0; m<filas2; m++)
+			{
+				for(int n=; n<columnas2; n++)
+				{
+			scanf("%d", &num);			
+		//metodo de la burbuja	
+		
+	}
+}
+
+	return 0;
+}
+
